fefecdd849ef8a916b01b87bb0d18a52d8efbf98
Alyxia Sother <alyxia@riseup.net>
2024-06-15 11:57:42 +0200

    [Meta] Add IDX config file

---
+++ .idx/dev.nix
@@ -0,0 +1,54 @@
+{ pkgs, ... }: {
+  channel = "stable-23.11";
+
+  packages = [
+    pkgs.inotify-tools
+    pkgs.gnupg
+
+    pkgs.nodejs_21
+    pkgs.nodePackages.pnpm
+    pkgs.dhall
+    pkgs.dhall-json
+    pkgs.dhall-lsp-server
+  ];
+
+  idx = {
+    extensions = [
+      # Meta
+      "jnoortheen.nix-ide"
+      "vscodevim.vim"
+
+      # Project
+      "astro-build.astro-vscode"
+      "unifiedjs.vscode-mdx"
+      "dhall.dhall-lang"
+      "dhall.vscode-dhall-lsp-server"
+      "nvarner.typst-lsp"
+    ];
+
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["pnpm" "dev" "--port" "9000"];
+          manager = "web";
+        };
+      };
+    };
+
+    workspace = {
+      onCreate = {
+        pnpm-install = "pnpm install";
+        gen-dhall = "dhall-to-json --file data/main.dhall --output data/data.json --pretty";
+      };
+      onStart = {
+        watch-dhall = ''
+          inotifywait -e close_write,moved_to,create -m data |
+          while read -r directory events filename; do
+            dhall-to-json --file data/main.dhall --output data/data.json --pretty
+          done
+        '';
+      };
+    };
+  };
+}
