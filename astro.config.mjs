7e1eec25717e8872e839796f5b76718d0af323e0
Chloe <chloe@sapphic.moe>
2024-12-17 02:51:19 +0500

    chore: fix the site config property

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: import.meta.env.PROD ? 'https://alyxia.dev' : 'http://localhost:4321',
   integrations: [mdx(), sitemap(), vue()]
 });
