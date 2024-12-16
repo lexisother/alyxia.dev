b2d90618c5596019df2dfb56b4c7a985ddd51701
Chloe <chloe@sapphic.moe>
2024-12-17 03:00:50 +0500

    chore: use node adapter and 'server' output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import node from '@astrojs/node';
 
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({ mode: 'standalone' }),
   site: import.meta.env.PROD ? 'https://alyxia.dev' : 'http://localhost:4321',
   integrations: [mdx(), sitemap(), vue()]
 });
