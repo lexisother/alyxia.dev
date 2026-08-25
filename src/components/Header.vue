970d2802b026b9e74fb15e45e345e9f17c15f2d8
Chloe A <chloe@sapphic.moe>
2026-08-25 14:26:48 +0500

    [components/Header] App Icon Webring

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -32,11 +32,30 @@ const IS_DEV = import.meta.env.DEV;
           </li>
         </ul>
       </template>
-      <script
-        v-if="!IS_DEV"
-        src="https://transring.neocities.org/ring.js"
-        data-widget="cloud"
-      ></script>
+
+      <div class="webrings">
+        <script
+          v-if="!IS_DEV"
+          src="https://transring.neocities.org/ring.js"
+          data-widget="cloud"
+        ></script>
+
+        <div class="badge app-icon-ring">
+          <a class="app-icon-ring-home" href="https://ring.basil.cafe">
+            <img width="88" height="31" src="https://ring.basil.cafe/images/88x31-nav/button.webp" alt="App Icon webring" />
+          </a>
+
+          <a class="app-icon-ring-arrow app-icon-ring-previous" href="https://ring.basil.cafe/prev?id=alyxia" aria-label="Previous site">
+            <img width="16" height="31" src="https://ring.basil.cafe/images/88x31-nav/left-hover.webp" alt="" />
+            <img width="16" height="31" src="https://ring.basil.cafe/images/88x31-nav/left-active.webp" alt="" />
+          </a>
+
+          <a class="app-icon-ring-arrow app-icon-ring-next" href="https://ring.basil.cafe/next?id=alyxia" aria-label="Next site">
+            <img width="16" height="31" src="https://ring.basil.cafe/images/88x31-nav/right-hover.webp" alt="" />
+            <img width="16" height="31" src="https://ring.basil.cafe/images/88x31-nav/right-active.webp" alt="" />
+          </a>
+        </div>
+      </div>
     </nav>
   </header>
 </template>
