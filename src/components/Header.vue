fa0fe0bc1a80beee6cce8b4d9bac21ff282428f2
Alyxia Sother <alyxia@riseup.net>
2026-08-28 23:00:06 +0200

    [components/AppIconWebring] Separate

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import xmark from "/static/img/xmark-solid.svg?url";
-import aly from "/static/img/aly.png?url";
-import { nav as hnavs } from "../../data/data.json";
 import { useToggle } from "@vueuse/core";
+import { nav as hnavs } from "../../data/data.json";
+import aly from "/static/img/aly.png?url";
+import xmark from "/static/img/xmark-solid.svg?url";
+import AppIconWebring from './AppIconWebring.vue';
 
 let [opened, toggleOpen] = useToggle(false);
 const IS_DEV = import.meta.env.DEV;
@@ -40,21 +41,7 @@ const IS_DEV = import.meta.env.DEV;
           data-widget="cloud"
         ></script>
 
-        <div class="badge app-icon-ring">
-          <a class="app-icon-ring-home" href="https://ring.basil.cafe">
-            <img width="88" height="31" src="https://ring.basil.cafe/images/88x31-nav/button.webp" alt="App Icon webring" />
-          </a>
-
-          <a class="app-icon-ring-arrow app-icon-ring-previous" href="https://ring.basil.cafe/prev?id=alyxia" aria-label="Previous site">
-            <img width="16" height="31" src="https://ring.basil.cafe/images/88x31-nav/left-hover.webp" alt="" />
-            <img width="16" height="31" src="https://ring.basil.cafe/images/88x31-nav/left-active.webp" alt="" />
-          </a>
-
-          <a class="app-icon-ring-arrow app-icon-ring-next" href="https://ring.basil.cafe/next?id=alyxia" aria-label="Next site">
-            <img width="16" height="31" src="https://ring.basil.cafe/images/88x31-nav/right-hover.webp" alt="" />
-            <img width="16" height="31" src="https://ring.basil.cafe/images/88x31-nav/right-active.webp" alt="" />
-          </a>
-        </div>
+        <AppIconWebring />
       </div>
     </nav>
   </header>
